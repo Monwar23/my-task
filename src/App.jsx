3aa1cfdd099c4985a11f79479c72b971d1baf86a
monwar23 <monwar2345@gmail.com>
2024-07-03 14:37:25 +0600

    create banner components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner"
 import Navbar from "./components/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
   <div className="max-w-6xl mx-auto">
     <Navbar></Navbar>
+    <Banner></Banner>
   </div>
     
   )
---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div className="my-10">
+            
+        </div>
+    );
+};
+
+export default Banner;
