044a72cee6906fe6158cb726a7a646d116d7c6a8
monwar23 <monwar2345@gmail.com>
2024-07-03 14:16:21 +0600

    initial setup with navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+  <div className="max-w-6xl mx-auto">
+    <Navbar></Navbar>
+  </div>
+    
+  )
+}
+
+export default App
